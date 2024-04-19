16b575900de5d35e221a1f3b7d2b77f5add3a9b0
Ofek Glazer <ofekglaz11@gmail.com>
2024-04-19 18:24:35 +0300

    create get recipe form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './Pages/Home';
+import './App.css'
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <div style={{ width: '100%', height: '100vh', backgroundColor: 'lightgray', display: "flex", justifyContent: "center", padding: 0, margin: 0 }}>
+        <div style={{ width: '70%', display: "flex", justifyContent: "center", backgroundColor: "white" }}>
+          <Home></Home>
+        </div>
+      </div>
     </>
   );
 }
